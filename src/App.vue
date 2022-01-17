39041dc068090b7fb5725bf0ffeb2f28c59567e3
pikminista <97889005+pikminista@users.noreply.github.com>
2022-01-17 21:27:48 +0900

    chore: add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <Calculator />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './pages/Calculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Calculator
+  },
+  data: () => ({
+    //
+  })
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
